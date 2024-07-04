29fbd1fd3d9e4cd96eb79da1424665744b16eba7
unknown <pabloowyh251@gmail.com>
2024-07-03 22:04:01 -0300

    Adicionando o resto

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
@@ -10,14 +11,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
       </header>
+      <HelloWorld />
     </div>
   );
 }
